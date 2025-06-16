a0adf46e16d306886c2f5c724a4b6e09e027a6b5
Nguyen Ngoc Tam <ngoctamsts1@gmail.com>
2025-06-16 14:02:21 +0700

    fix api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function RegisterPage() {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:3000/api/register", {
+      const response = await fetch("https://two21110155-nguyenngoctam.onrender.com/api/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
